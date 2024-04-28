eb67e86479913d0a0224f84408ca4d340abf8df1
jin <jin337x@outlook.com>
2024-04-28 19:25:39 +0800

    updata

--- src/stores/shop.ts
+++ src/stores/shop.ts
@@ -202,6 +202,114 @@ export const useShopStore = defineStore("shop", {
         啦啦啦
         啦啦啦啦啦啦啦`,
       },
+      {
+        id: 5,
+        backImg: [
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/18629/34/3378/144318/5c263f64Ef0e2bff0/0d650e0aa2e852ee.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/26597/30/4870/174583/5c35c5d2Ed55eedc6/50e27870c25e7a82.png",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/9542/17/12873/201687/5c3c4362Ea9eb757d/60026b40a9d60d85.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/30042/36/427/82951/5c3bfdabE3faf2f66/9adca782661c988c.jpg",
+        ],
+        price: 43,
+        vip: 39.9,
+        count: 12,
+        title: "555润本柠檬贴30条/袋",
+        sub: "植物精油帖，防水止汗",
+        dec: `啦啦啦啦
+        啦啦啦
+        啦啦啦
+        啦啦啦啦啦啦啦`,
+      },
+      {
+        id: 6,
+        backImg: [
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/18629/34/3378/144318/5c263f64Ef0e2bff0/0d650e0aa2e852ee.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/26597/30/4870/174583/5c35c5d2Ed55eedc6/50e27870c25e7a82.png",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/9542/17/12873/201687/5c3c4362Ea9eb757d/60026b40a9d60d85.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/30042/36/427/82951/5c3bfdabE3faf2f66/9adca782661c988c.jpg",
+        ],
+        price: 43,
+        vip: 39.9,
+        count: 12,
+        title: "666润本柠檬贴30条/袋",
+        sub: "植物精油帖，防水止汗",
+        dec: `啦啦啦啦
+        啦啦啦
+        啦啦啦
+        啦啦啦啦啦啦啦`,
+      },
+      {
+        id: 7,
+        backImg: [
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/18629/34/3378/144318/5c263f64Ef0e2bff0/0d650e0aa2e852ee.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/26597/30/4870/174583/5c35c5d2Ed55eedc6/50e27870c25e7a82.png",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/9542/17/12873/201687/5c3c4362Ea9eb757d/60026b40a9d60d85.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/30042/36/427/82951/5c3bfdabE3faf2f66/9adca782661c988c.jpg",
+        ],
+        price: 43,
+        vip: 39.9,
+        count: 12,
+        title: "777润本柠檬贴30条/袋",
+        sub: "植物精油帖，防水止汗",
+        dec: `啦啦啦啦
+        啦啦啦
+        啦啦啦
+        啦啦啦啦啦啦啦`,
+      },
+      {
+        id: 8,
+        backImg: [
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/18629/34/3378/144318/5c263f64Ef0e2bff0/0d650e0aa2e852ee.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/26597/30/4870/174583/5c35c5d2Ed55eedc6/50e27870c25e7a82.png",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/9542/17/12873/201687/5c3c4362Ea9eb757d/60026b40a9d60d85.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/30042/36/427/82951/5c3bfdabE3faf2f66/9adca782661c988c.jpg",
+        ],
+        price: 43,
+        vip: 39.9,
+        count: 12,
+        title: "888润本柠檬贴30条/袋",
+        sub: "植物精油帖，防水止汗",
+        dec: `啦啦啦啦
+        啦啦啦
+        啦啦啦
+        啦啦啦啦啦啦啦`,
+      },
+      {
+        id: 9,
+        backImg: [
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/18629/34/3378/144318/5c263f64Ef0e2bff0/0d650e0aa2e852ee.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/26597/30/4870/174583/5c35c5d2Ed55eedc6/50e27870c25e7a82.png",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/9542/17/12873/201687/5c3c4362Ea9eb757d/60026b40a9d60d85.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/30042/36/427/82951/5c3bfdabE3faf2f66/9adca782661c988c.jpg",
+        ],
+        price: 43,
+        vip: 39.9,
+        count: 12,
+        title: "999润本柠檬贴30条/袋",
+        sub: "植物精油帖，防水止汗",
+        dec: `啦啦啦啦
+        啦啦啦
+        啦啦啦
+        啦啦啦啦啦啦啦`,
+      },
+      {
+        id: 10,
+        backImg: [
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/18629/34/3378/144318/5c263f64Ef0e2bff0/0d650e0aa2e852ee.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/26597/30/4870/174583/5c35c5d2Ed55eedc6/50e27870c25e7a82.png",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/9542/17/12873/201687/5c3c4362Ea9eb757d/60026b40a9d60d85.jpg",
+          "//m.360buyimg.com/mobilecms/s750x366_jfs/t1/30042/36/427/82951/5c3bfdabE3faf2f66/9adca782661c988c.jpg",
+        ],
+        price: 43,
+        vip: 39.9,
+        count: 12,
+        title: "101010润本柠檬贴30条/袋",
+        sub: "植物精油帖，防水止汗",
+        dec: `啦啦啦啦
+        啦啦啦
+        啦啦啦
+        啦啦啦啦啦啦啦`,
+      },
     ],
     shopCart: [],
   }),
